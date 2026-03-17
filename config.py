a850822ab111d89cdb3288ba20e9395f247cf6bf
Jason S. <J23kirby@gmail.com>
2026-03-17 02:19:05 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+DATABASE_FOLDER = "database_folder"
+DATABASE_FILE = os.path.join(DATABASE_FOLDER, "database.csv")
+
+CHECKIN_FOLDER = "checkin_logs"
+TERMS_FILE = "terms_and_conditions.txt"
+PHOTO_FOLDER = "checkin_photos"
+
+ADMIN_USERNAME = "gay"
+ADMIN_PASSWORD = "gay"
+
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 550
