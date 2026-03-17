76f1d3e24fbdbe1c1d3f9127a2581fa2add5202e
Jason S. <J23kirby@gmail.com>
2026-03-17 01:03:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DATABASE_FILE = os.path.join(DATABASE_FOLDER, "database.csv")
 CHECKIN_FOLDER = "checkin_logs"
 TERMS_FILE = "terms_and_conditions.txt"
 
-ADMIN_USERNAME = "gay"
-ADMIN_PASSWORD = "gay"
+ADMIN_USERNAME = "adminusername" # PLEASE CHANGE 
+ADMIN_PASSWORD = "adminpassword" # PLEASE CHANGE
 
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 550
